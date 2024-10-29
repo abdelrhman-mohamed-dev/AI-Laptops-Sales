a9ffa13cd6b6689fd67cc00851e36b646f008065
Abdelrhman Mohamed <mohamedsaad81973@gmail.com>
2024-10-29 11:49:58 +0300

    edit nex.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "api/rag",
+          destination: `https://ai-laptops.netlify.app/api/rag`,
+        },
+      ],
+    };
+  },
+};
 export default nextConfig;
